a684df964bba7e0cb70313844107719e1458a2e7
Erisson Nunes <42328316+bn-erisson@users.noreply.github.com>
2020-12-13 22:58:41 -0300

    Resolvi o erro nas extençoes dos arquivos

--- Atividades/projeto_1/components/Corpo.js
+++ Atividades/projeto_1/components/Corpo.js
@@ -3,7 +3,7 @@ import {Image} from 'react-native';
 
 export default class Corpo extends Component{
   render(){
-    let pic = {uri:'https://mir-s3-cdn-cf.behance.net/user/276/4fabd856746153.5ac527b8ac12d.jpg'};
+    let pic = {uri:'https://mir-s3-cdn-cf.behance.net/user/230/0ee5e456746153.5fd3a421ee847.jpg'};
       return (
         <Image source={pic} style={{width:113,height:150}}/>
       );
